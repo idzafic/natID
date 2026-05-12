a36c842191000da48279e4f0d0ae1732896ade27
idzafic <idzafic@etf.unsa.ba>
2026-05-12 07:55:49 +0200

    Matrix header fix
    Matrix header fix

---
+++ natID.SDK/Common/Include/BLAS/Platform.h
@@ -0,0 +1,14 @@
+// ################################################################################################################
+// # native Interface Design (natID)
+// # Licensed under the Creative Commons Attribution-NoDerivatives (CC BY-ND), version 4.
+// # 
+// # You may use this code under the terms of the Creative Commons Attribution-NoDerivatives (CC BY-ND), version 4.
+// # 
+// # Contact: idzafic at etf.unsa.ba  or idzafic at gmail.com
+// ################################################################################################################
+
+#pragma once
+namespace BLAS
+{
+	typedef enum {AMD_Athlon=0, AMD_Barcelona=1, Intel_Pentium4=100, Intel_Core2=101, Intel_Penryn=102, Intel_Nehalem=103} Platform;
+}
---
+++ natID.SDK/Common/Include/matrix/MatrixLib.h
@@ -0,0 +1,31 @@
+// ################################################################################################################
+// # native Interface Design (natID)
+// # Licensed under the Creative Commons Attribution-NoDerivatives (CC BY-ND), version 4.
+// # 
+// # You may use this code under the terms of the Creative Commons Attribution-NoDerivatives (CC BY-ND), version 4.
+// # 
+// # Contact: idzafic at etf.unsa.ba  or idzafic at gmail.com
+// ################################################################################################################
+
+#pragma once
+#include <td/Types.h>
+#include <compiler/Definitions.h>
+
+#ifdef MU_WINDOWS
+    #ifdef MATRIX_EXPORTS
+        #define MATRIX_API __declspec(dllexport)
+        #define MATRIX_EXTERN
+    #else
+        #define MATRIX_API __declspec(dllimport)
+        #define MATRIX_EXTERN extern
+    #endif
+#else
+    #ifdef MATRIX_EXPORTS
+        #define MATRIX_API __attribute__((visibility("default")))
+        #define MATRIX_EXTERN
+    #else
+        #define MATRIX_API
+        #define MATRIX_EXTERN extern
+    #endif
+#endif
+
